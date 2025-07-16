c21790f8178cb3ff0733a27fbf3b101e48d1dd0e
Devituz <shohbozbek.uz24@gamil.com>
2025-07-16 05:41:14 -0700

    tugadi

--- app/Http/Controllers/AllController.php
+++ app/Http/Controllers/AllController.php
@@ -14,7 +14,7 @@ class AllController extends Controller
 {
     public function index()
     {
-        $phone = request()->query('phone', '+998889442402');
+        $phone = request()->query('phone', '+998908212776');
         $normalizedPhone = preg_replace('/[^0-9]/', '', $phone);
 
         $botUser = BotUser::whereRaw("REPLACE(REPLACE(REPLACE(phone, '+', ''), ' ', ''), '-', '') = ?", [$normalizedPhone])->first();
